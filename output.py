c592ac71928db6202a1d820c57ab3e80369e0bd7
Ccode-lang <coop112097@gmail.com>
2022-12-02 18:04:54 -0700

    add output console

---
+++ output.py
@@ -0,0 +1,18 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 5002))
+s.listen(1)
+connected = False
+
+while True:
+    if not connected:
+        conn, addr = s.accept()
+        connected = True
+    data = conn.recv(1024)
+    
+    if not data:
+        connected = False
+        conn.close()
+
+    if connected:
+        print(data.decode())
--- robot.py
+++ robot.py
@@ -1,20 +1,33 @@
+import socket
+from time import sleep
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(0.2)
+
+
 #define your variables here
 
 
 counter = 0
+
+server_address = ""
 
 
 
 
 def mainloop():
     global counter
-    print(str(counter))
+    sleep(.07)
+    s.send(str(counter).encode())
     counter += 1
 
 def before(addr):
     print(addr)
+    global server_address
+    server_address = (addr[0], 5002)
     global counter
     counter = 0
+    s.connect(server_address)
+    s.send("hello".encode())
 
 def loops(arg):
     return False
